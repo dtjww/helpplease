56d98e486c7e87c192065faec03188535afa3cb9
renee7777 <reneelim.2021@scis.smu.edu.sg>
2022-10-13 21:14:51 +0800

    NavBar implemented in AboutView.vue

--- mortalportal/src/components/NavBar.vue
+++ mortalportal/src/components/NavBar.vue
@@ -1,12 +1,96 @@
 <!-- Renee -->
-<template>
+<!-- <template>
     <div class="nav-bar">
-    <div class="nav-bar__logo">
-        <img src="../assets/MainLogo.png" alt="logo" />
+        <div class="nav-bar__logo">
+            <img src="../assets/MainLogo.png" alt="logo" />
+        </div>
+        <div class="nav-bar__links">
+            <a href="#">Saved</a>
+            <a href="#">Notifications</a>
+            <a href="#">Top Angels</a>
+            <a href="#">Log In</a>
+            <p>/</p>
+            <a href="#">Sign Up</a>
+        </div>
     </div>
-    <div class="nav-bar__links">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+</template>
+
+<style>
+    .nav-bar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 5%;
+        height: 10vh;
+        background-color: #f5f5f5;
+        border-bottom: 1px solid #e0e0e0;
+    }
+</style> -->
+
+<template>
+    <div class="q-pa-md">
+        <q-toolbar class="navbar-bg text-black q-my-md shadow-2">
+            <q-btn flat round dense icon="menu" class="q-mr-sm" />
+            <q-separator dark vertical inset />
+            <q-btn stretch flat label="Home" />
+    
+            <q-space />
+    
+            <q-btn stretch flat label="Saved" />
+            <q-separator dark vertical />
+
+            <q-btn stretch flat label="Notifications" />
+            <q-separator dark vertical />
+            
+            <q-btn stretch flat label="Top Angels" />
+            <q-separator dark vertical />
+            
+            <q-btn stretch flat label="Log In" />
+            <q-separator dark vertical />
+            
+            <q-btn stretch flat label="Sign Up" />
+            <q-separator dark vertical />
+
+            <q-btn-dropdown stretch flat label="Profile">
+                <q-list>
+                    <q-item-label header>Profile</q-item-label>
+                    <q-item v-for="n in 3" :key="`x.${n}`" clickable v-close-popup tabindex="0">
+                    <q-item-section avatar>
+                        <q-avatar icon="folder" color="secondary" text-color="white" />
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>My Account</q-item-label>
+                        <q-item-label caption>Account Settings</q-item-label>
+                    </q-item-section>
+                    <q-item-section side>
+                        <q-icon name="info" />
+                    </q-item-section>
+                    </q-item>
+
+                    <q-separator inset spaced />
+                    
+                    <!-- <q-item-label header>Files</q-item-label>
+                    <q-item v-for="n in 3" :key="`y.${n}`" clickable v-close-popup tabindex="0">
+                    <q-item-section avatar>
+                        <q-avatar icon="assignment" color="primary" text-color="white" />
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>Vacation</q-item-label>
+                        <q-item-label caption>February 22, 2016</q-item-label>
+                    </q-item-section>
+                    <q-item-section side>
+                        <q-icon name="info" />
+                    </q-item-section>
+                    </q-item> -->
+                </q-list>
+            </q-btn-dropdown>
+
+        </q-toolbar>
     </div>
-  </div>
+</template>
+
+<style>
+    .navbar-bg {
+        background-color: #eaac8b;
+    }
+</style>
--- mortalportal/src/views/AboutView.vue
+++ mortalportal/src/views/AboutView.vue
@@ -1,5 +1,19 @@
+<!--Renee testing NavBar output here-->
 <template>
+  <NavBar msg="This is my NavBar bro"/>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+  // @ is an alias to /src
+  import NavBar from '@/components/NavBar.vue'
+
+  export default {
+    name: 'AboutView',
+    components: {
+      NavBar
+    }
+  }
+</script>
