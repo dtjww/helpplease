8e213c36219eb577eb49a3b5cff7d33f639ced7d
merweeeeen <melvinngchuanjie@gmail.com>
2022-10-13 18:40:20 +0800

    dir

--- mortalportal/src/router/index.js
+++ mortalportal/src/router/index.js
@@ -1,19 +1,59 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/NewUserHome.vue'
 
 const routes = [
   {
-    path: '/',
+    path: '/', // NewUserHome
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
+  { 
+    path: '/about', // About Page
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/home', // OurHome
+    name: 'loginhome',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/OurHome.vue')
+  },
+  {
+    path: '/chat', // ChatPage
+    name: 'chatpage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Chatpage.vue')
+  },
+  {
+    path: '/profile', // Profile
+    name: 'Profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Profilepage.vue')
+  },
+  {
+    path: '/signup', // Sign Up
+    name: 'signup',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignUp.vue')
+  },
+  {
+    path: '/task', // Task Page
+    name: 'task',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Taskpage.vue')
   }
 ]
 
