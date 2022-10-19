a16997d542be6c48e38ce4a6f07b98707cd24226
anirbanter <jeettheman24@gmail.com>
2022-10-19 17:15:47 +0800

    got rid of firebase stuff

--- mortalportal/src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyDv80szMnvKoQkFKEqCCpZkANoVTXpUXgE",
-    authDomain: "fir-login-ee25d.firebaseapp.com",
-    projectId: "fir-login-ee25d",
-    storageBucket: "fir-login-ee25d.appspot.com",
-    messagingSenderId: "95685694990",
-    appId: "1:95685694990:web:6283d7e2d2e738cfba0e5f",
-    measurementId: "G-6WLV0MJHJM"
-  };
