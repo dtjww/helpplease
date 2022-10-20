7aaa678f261d0d25e615d39197eb9b1740d533da
merweeeeen <melvinngchuanjie@gmail.com>
2022-10-20 22:23:25 +0800

    taskbar

--- mortalportal/src/router/index.js
+++ mortalportal/src/router/index.js
@@ -45,7 +45,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignUp.vue')
   },
   {
     path: '/task', // Task Page
--- mortalportal/src/views/TaskpageView.vue
+++ mortalportal/src/views/TaskpageView.vue
@@ -2,18 +2,76 @@
 
 <template>
     <NavBar/>
-    <div>
-        <form>
-            <q-card class="card">
-            <h6>Category of Task</h6>
-            <q-select outlined v-model="model" :options="task" label="Choose a Task" class="inputboxes" />
-            <label>Name of Task</label>
-            <input v-model="title" placeholder="Name of Tasks" maxlength="30" class="inputboxes">
-            <h6>Price</h6>
-            <input v-model="title" placeholder="Price" maxlength="30" class="inputboxes">
 
-        </q-card>
+    <h3>Create a New Task</h3>
+    <div><q-card class="card">
+        <form>
+            
+            <table>
+                <tr>
+                    <td><h6>Category of Task</h6></td>
+                </tr>
+                <tr>
+                    <td><q-select outlined v-model="category" :options="task" label="Choose a Task" class="inputboxes" /></td>
+                </tr>
+
+                <tr>
+                    <td><h6>Name of Task</h6></td>
+                </tr>
+                <tr>
+                    <td><q-input outlined v-model="name" placeholder="Name of Task" maxlength="30" class="inputboxes"/></td>
+                </tr>
+
+                <tr>
+                    <td><h6>Description of Task</h6></td>
+                </tr>
+                <tr>
+                    <td><q-input outlined v-model="desc" placeholder="Description of Task" maxlength="30" class="inputboxes"/></td>
+                </tr>
+
+                <tr>
+                    <td><h6>Price</h6></td>
+                </tr>
+                <tr>
+                    <td>
+                        <q-input outlined v-model="price" placeholder="Price" maxlength="30" class="price" prefix='$'/>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><h6>Location</h6></td>
+                </tr>
+                <tr>
+                    <td><q-input outlined v-model="loc" placeholder="Location of Task" maxlength="30" class="inputboxes"/></td>
+                </tr>
+
+                <tr>
+                    <td><h6>Date & Time</h6></td>
+                </tr>
+                <tr>
+                        <td>
+                            <q-input v-model="time" outlined type="time" class='date'/> 
+                            <q-input v-model="date" outlined type="date" class="time"/>
+                        </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <h6>Image (Optional)</h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <q-input
+                            @update:model-value="val => { file = val[0] }"
+                            outlined
+                            type="file"/>
+                    </td>
+                </tr>
+            </table>
+            <q-btn color="white" text-color="black" label="Post Task" class='button' @click=submitForm() />
         </form>
+    </q-card>
     </div>
 
 </template>
@@ -21,20 +79,59 @@
 <script>
 import { ref } from 'vue'
 import NavBar from '@/components/NavBar.vue'
+// import { firebaseConfig } from '@/components/FirebasePush.vue';
+// import * as firebase from 'firebase';
+// import { initializeApp } from "firebase/app";
 
+// firebase.initializeApp(firebaseConfig);
+// var DreemTeemDB = firebase.database().ref('DreemTeem');
+  
+  
 export default {
-  setup () {
+  data () {
     return {
       model: ref(null),
       task: [
         'Shopping', 'Physical', 'Assignment/Project', 'Hardware/Software'
-      ]
+      ],
+      time: ref(''),
+      date: ref(''),
+      loc: ref(''),
+      name: ref(''),
+      desc: ref(''),
+      category: ref(''),
+      file: ref(null),
+      files: ref(null),
+      price: ref('')
     }
   },
   components:{
         NavBar
-  }
+  },
+//   methods:{
+//         saveDB(Amount, Data, Time, Task){
+        
+//         var newForm = DreemTeemDB.push();
+//         newForm.set({
+//         Amount: Amount,
+//         Date: Date,
+//         Time: Time,
+//         Task: Task
+//             })
+//         },
+//         submitForm(){
+//             var Amount = this.price;
+//             var Date = this.date;
+//             var Time = this.time;
+//             var Task = this.name;
+//             this.saveDB(Amount, Date, Time, Task);
+//         },
+    
+    
+    // }
+
 }
+
 </script>
 
 <style>
@@ -43,7 +140,8 @@ export default {
     width:700px;
     margin-left:auto;
     margin-right:auto;
-    padding:10px;
+    margin-bottom: 50px;
+    padding:50px;
 
 }
 h6{
@@ -52,13 +150,37 @@ h6{
 }
 
 .inputboxes{
-    width:300px;
+    width:600px;
     margin-top:10px;
     margin-bottom: 10px;
     margin-left:0px;
-    text-align:left;
+    text-align:center;
     
 }
 
+.price{
+    width:100px;
+    text-align:center;
 
+}
+
+.date{
+    width:250px;
+    float:left;
+    margin-right:5px;
+}
+
+.time{
+    width:250px;
+    float:left;
+    margin-left:5px;
+}
+
+h3{
+    margin:auto
+}
+
+.button{
+    margin:15px;
+}
 </style>
