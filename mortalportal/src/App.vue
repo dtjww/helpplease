9200beb061fc06d9b551a30432ade9cde8855e6d
merweeeeen <melvinngchuanjie@gmail.com>
2022-10-20 15:48:37 +0800

    navbar

--- mortalportal/src/App.vue
+++ mortalportal/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<NavBar/> 
   <router-view/>
+  
 </template>
+<script>
+  import NavBar from '@/components/NavBar.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      NavBar
+    }
+  }
+</script>
 
 <style>
 #app {
