84469a8ef5f14bcc344d97e66b13ebc9daba3217
renee7777 <reneelim.2021@scis.smu.edu.sg>
2022-10-26 09:55:51 +0800

    Responsive Navbar

--- mortalportal/src/views/AboutView.vue
+++ mortalportal/src/views/AboutView.vue
@@ -6,19 +6,11 @@
       height: 100vh;
     ">
     <NavBar/>
-    <BackgroundImage/>
     <h1>This is an about page</h1>
     <NavBarLogIn/>
     <TaskComplete/>
   </div>
   
-  <!-- <div
-    class="bg-image"
-    style="
-      background-image: url('https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp');
-      height: 100vh;
-    "
-  ></div> -->
 </template>
 
 <script>
@@ -26,16 +18,13 @@
   import NavBar from '@/components/NavBar.vue'
   import NavBarLogIn from '@/components/NavBarLogIn.vue'
   import TaskComplete from '@/components/TaskComplete.vue'
-  import BackgroundImage from '@/components/BackgroundImage.vue'
-
 
   export default {
     name: 'AboutView',
     components: {
       NavBar,
       NavBarLogIn,
-      TaskComplete,
-      BackgroundImage
+      TaskComplete
     }
   }
 </script>
