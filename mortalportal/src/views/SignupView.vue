746dfb3a2a20f573f43fb81892952639c45844d9
merweeeeen <melvinngchuanjie@gmail.com>
2022-11-09 01:20:04 +0800

    google login

--- mortalportal/src/views/SignupView.vue
+++ mortalportal/src/views/SignupView.vue
@@ -1,32 +1,58 @@
 <!-- daryl-->
 <template>
-  <div class="row img-overlay img-wrapper"> 
-        <div class="column">
-            <img class="image" src="https://images.unsplash.com/photo-1605826832916-d0ea9d6fe71e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1628435600697-6a954dea5055?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1576613109753-27804de2cba8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" style="width:100%"/>
-        </div>
-        
-        <div class="column">
-            <img class="image" src="https://images.unsplash.com/photo-1558975285-193b2c315c2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1630432328419-bee5f50d6390?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1632345031435-8727f6897d53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" style="width:100%"/>
-        </div> 
-        
-        <div class="column">
-            <img class="image" src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2059&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1582902281043-69c645f40cd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1623348282113-50395974159c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" style="width:100%"/>
-        </div>
-        
-        <div class="column">
-            <img class="image" src="https://images.unsplash.com/photo-1516386408840-d718a820a05c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1568515045052-f9a854d70bfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" style="width:100%"/>
-            <img class="image" src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" style="width:100%"/>
-            
-        </div>
+  <div class="row img-overlay img-wrapper">
+    <div class="column">
+      <img class="image"
+        src="https://images.unsplash.com/photo-1605826832916-d0ea9d6fe71e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1628435600697-6a954dea5055?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1576613109753-27804de2cba8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+        style="width:100%" />
     </div>
+
+    <div class="column">
+      <img class="image"
+        src="https://images.unsplash.com/photo-1558975285-193b2c315c2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1630432328419-bee5f50d6390?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1632345031435-8727f6897d53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+        style="width:100%" />
+    </div>
+
+    <div class="column">
+      <img class="image"
+        src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2059&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1582902281043-69c645f40cd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1623348282113-50395974159c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        style="width:100%" />
+    </div>
+
+    <div class="column">
+      <img class="image"
+        src="https://images.unsplash.com/photo-1516386408840-d718a820a05c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1568515045052-f9a854d70bfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        style="width:100%" />
+      <img class="image"
+        src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+        style="width:100%" />
+
+    </div>
+  </div>
 
   <q-layout>
     <q-page-container>
@@ -47,6 +73,7 @@
                 <p class="text-grey-6">Not registered? Sign up for an account!</p>
               </q-card-section>
 
+
               <q-card-section>
                 <q-form class="q-gutter-md">
                   <q-input square filled clearable v-model="email_login" type="text" label="Email" />
@@ -61,14 +88,39 @@
             <q-tab-panel name="Sign Up">
               <q-card-section class="text-center q-pa-none">
                 <p class="text-grey-6">Already have an account? Log in!</p>
+
+                <!-- <div class="googleLogin">
+
+                  <h6 v-if="(getGoogleAuth().user) == null" class="text-white">Signed In User: {{ getGoogleAuth().user}}</h6>
+                  <div id="firebaseui-auth-container"></div>
+                  <div id="loader">Loading...</div>
+                  <div v-if="isSignedIn">
+                    <button @click="handleSignOut">Sign Out</button>
+                  </div>
+                </div> -->
+
+                <div class="googleLogin">
+                  
+                  <h6 v-if="lUser != null" class="text-black">{{ lUser }}</h6>
+                  <div id="firebaseui-auth-container"></div>
+                  <div id="loader">Loading...</div>
+                  <div v-if="isSignedIn">
+                    <button @click="getGoogleAuth().handleSignOut">Sign Out</button>
+                  </div>
+                </div>
+
+                <!-- <q-btn @click="handleGoogleLogin">
+                  Log in with Google
+                </q-btn> -->
+
               </q-card-section>
               <q-card-section>
                 <q-form class="q-gutter-md">
-                  <q-input square filled clearable v-model="username_SU" type="text" label="Username" />
-                  <q-input square filled clearable v-model="name_SU" type="text" label="Full Name" />
-                  <q-input square filled clearable v-model="password_SU" type="password" label="Password" />
-                  <q-input square filled clearable v-model="cPassword_SU" type="password" label="Confirm Password" />
-                  <q-input square filled clearable v-model="email_SU" type="email" label="Email" />
+                  <q-input square filled clearable v-model="username" type="text" label="Username" />
+                  <q-input square filled clearable v-model="name" type="text" label="Full Name" />
+                  <q-input square filled clearable v-model="password" type="password" label="Password" />
+                  <q-input square filled clearable v-model="cPassword" type="password" label="Confirm Password" />
+                  <q-input square filled clearable v-model="email" type="email" label="Email" />
                 </q-form>
               </q-card-section>
               <q-card-actions class="q-px-md">
@@ -108,14 +160,14 @@
   </q-page> -->
 
   <!-- Google login code is in start and end comments, only works on localhost for now -->
-  <div class="googleLogin">
-    <h6 v-if="user" class="text-white">Signed In User: {{ user }}</h6>
+  <!-- <div class="googleLogin">
+    <h6 v-if="isSignedIn.value" class="text-white">Signed In User: {{ lUser }}</h6>
     <div id="firebaseui-auth-container"></div>
     <div id="loader">Loading...</div>
     <div v-if="isSignedIn">
       <button @click="handleSignOut">Sign Out</button>
     </div>
-  </div>
+  </div> -->
 </template>
 
 
@@ -129,6 +181,7 @@ firebase.initializeApp(firebaseConfig);
 import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
 import { getAuth, signOut } from "firebase/auth";
+// import { getAuth, GoogleAuthProvider, signInWithPopup,signOut } from "firebase/auth";
 const auth = getAuth();
 // google login end
 
@@ -136,34 +189,32 @@ const auth = getAuth();
 import { useCounterStore } from "@/store/store";
 import axios from "axios";
 import { db } from '../firebase.js';
-import {set, ref as dbRef } from "firebase/database";
+import { set, ref as dbRef } from "firebase/database";
 const storeName = useCounterStore()
 export default {
   setup() {
-
-    
-
     return {
       tab: ref('Sign Up')
-
     }
   },
   data() {
     return {
       email_login: '',
       password_login: '',
-      username_SU: '',
-      name_SU: '',
-      password_SU: '',
-      cPassword_SU: '',
-      email_SU: '',
+      username: '',
+      name: '',
+      password: '',
+      cPassword: '',
+      email: '',
       users: [],
       signupData: {
         email: '',
         password: '',
         username: '',
-        name: ''
-      }
+        name: '',
+      },
+      lUser: ref(null),
+      isSignedIn: ref(false)
     }
   },
 
@@ -195,88 +246,89 @@ export default {
         })
     },
     submitData() {
-      if (this.password_SU == this.cPassword_SU) {
-        this.signupData.email = this.email_SU
-        this.signupData.password = this.password_SU
-        this.signupData.username = this.username_SU
-        this.signupData.name = this.name_SU
+      if (this.password == this.cPassword) {
+        this.signupData.email = this.email
+        this.signupData.password = this.password
+        this.signupData.username = this.username
+        this.signupData.name = this.name
       }
       else {
         alert("Passwords do not match")
       }
-      set(dbRef(db, 'Login/'+ this.signupData.username), this.signupData);
+      set(dbRef(db, 'Login/' + this.signupData.username), this.signupData);
       storeName.username = this.signupData.username;
       storeName.email = this.signupData.email
       this.$router.push('/home/angel');
     },
 
-    getGoogleAuth ()    {
+    getGoogleAuth() {
       // google login start
-    const user = ref(null);
-    const isSignedIn = ref(false);
-    const uiConfig = {
-      signInFlow: 'popup',
-      signinSuccessUrl: 'http://localhost:8080/',
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      ],
-      callbacks: {
-        signInSuccessWithAuthResult: function (authResult) {
-          user.value = authResult.user.displayName;
-          console.log(authResult)
-          isSignedIn.value = true;
-          console.log('Signed in by user ' + user.value);
-          // so it doesn't refresh the page
-          return false;
-        },
-        uiShown: function () {
-          // The widget is rendered.
-          // Hide the loader.
-          document.getElementById('loader').style.display = 'none';
+      const user = ref(null);
+      const email = ref(null);
+      const isSignedIn = ref(false);
+      const uiConfig = {
+        signInFlow: 'popup',
+        signinSuccessUrl: 'gSignin',
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+          signInSuccessWithAuthResult: function (authResult) {
+            user.value = authResult.user.displayName;
+            email.value = authResult.user.email;
+            isSignedIn.value = true;
+            console.log('Signed in by user ' + user.value);
+            storeName.name = user.value
+            storeName.email = email.value
+
+            // so it doesn't refresh the page
+            return true;
+          },
+          uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+          },
         }
       }
-    }
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start('#firebaseui-auth-container', uiConfig);
-    const handleSignOut = () => {
-      signOut(auth).then(() => {
-        // Sign-out successful.
-        user.value = null;
-        isSignedIn.value = false;
-        console.log('Signed out');
-        ui.start('#firebaseui-auth-container', uiConfig);
-      }).catch((error) => {
-        // An error happened.
-        console.log(error);
-      });
-    }
-    // google login end
-    return {
-      // google login start
-      user,
-      isSignedIn,
-      handleSignOut,
-      // google login end
-    }
 
-    }
-    // valLogin(){
-    //   console.log(this.users)
-    //   for (var user of this.users){
-    //     if (user.email == this.email_login && user.password == this.password_login){
-    //       this.login();
-    //     }
-    //     else {
-    //       alert("Incorrect email or password")
-    //     }
-    //   }
-    // }
-    
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start('#firebaseui-auth-container', uiConfig);
+      console.log()
+      const handleSignOut = () => {
+        signOut(auth).then(() => {
+          // Sign-out successful.
+          user.value = null;
+          isSignedIn.value = false;
+          console.log('Signed out');
+          this.isSignedIn = false;
+          ui.start('#firebaseui-auth-container', uiConfig);
+        }).catch((error) => {
+          // An error happened.
+          console.log(error);
+        });
+      }
+      
+
+
+      // google login end
+      return {
+        // google login start
+        user,
+        isSignedIn,
+        handleSignOut,
+
+        // google login end
+      }}
+      ,
+
   },
   mounted:
     function () {
       this.getGoogleAuth()
+      this.lUser = this.getGoogleAuth().user
+      this.isSignedIn = this.getGoogleAuth().isSignedIn
     }
 }
 
@@ -293,74 +345,74 @@ export default {
 } */
 
 * {
-        box-sizing: border-box;
-    }
+  box-sizing: border-box;
+}
 
-    body {
-        margin: 0;
-        font-family: Arial;
-    }
+body {
+  margin: 0;
+  font-family: Arial;
+}
 
-    .header {
-        text-align: center;
-        padding: 32px;
-    }
+.header {
+  text-align: center;
+  padding: 32px;
+}
 
-    .row {
-        display: flex;
-        flex-wrap: wrap;
-        padding: 0 4px;
-    }
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 4px;
+}
 
-    /* Create four equal columns that sits next to each other */
-    .column {
-        flex: 25%;
-        max-width: 25%;
-        padding-left: 5px;
-        padding-right: 5px;
-        /* max-height: 100vh; */
-    }
+/* Create four equal columns that sits next to each other */
+.column {
+  flex: 25%;
+  max-width: 25%;
+  padding-left: 5px;
+  padding-right: 5px;
+  /* max-height: 100vh; */
+}
 
-    .column img {
-        margin-top: 8px;
-        vertical-align: middle;
-    }
+.column img {
+  margin-top: 8px;
+  vertical-align: middle;
+}
 
-    .image {
-        padding-bottom: 5px;
-        border-radius: 15px;
-        opacity: 0.8;
-    }
+.image {
+  padding-bottom: 5px;
+  border-radius: 15px;
+  opacity: 0.8;
+}
 
-    .img-wrapper {
-        position: relative;
-        width: 100%;
-        font-size: 0;
-    }
+.img-wrapper {
+  position: relative;
+  width: 100%;
+  font-size: 0;
+}
 
-    .img-overlay {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.86);
-    }
+.img-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.86);
+}
 
 
-    /* Responsive layout - makes a two column-layout instead of four columns */
-    @media (max-width: 800px) {
-        .column {
-            flex: 50%;
-            max-width: 50%;
-        }
-    }
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media (max-width: 800px) {
+  .column {
+    flex: 50%;
+    max-width: 50%;
+  }
+}
 
-    /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
-    @media (max-width: 600px) {
-        .column {
-            flex: 100%;
-            max-width: 100%;
-        }
-    }
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media (max-width: 600px) {
+  .column {
+    flex: 100%;
+    max-width: 100%;
+  }
+}
 </style>
