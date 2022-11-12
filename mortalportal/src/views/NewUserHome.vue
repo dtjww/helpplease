518b255f46b9b876b18158ad0b9cf9608f7c8335
renee7777 <reneelim.2021@scis.smu.edu.sg>
2022-11-12 16:56:36 +0800

    NewUserHome

--- mortalportal/src/views/NewUserHome.vue
+++ mortalportal/src/views/NewUserHome.vue
@@ -14,10 +14,7 @@
         <q-page>
           <div style="margin:0%;padding:0px">
             <div>
-              <div class="container-2 font" style="position:absolute;width:100%;height:100vh;z-index:2">
-                <div class="text-h1 bg-transparent" :class="textColor" style="position:absolute;top:70;z-index:3">Lend a
-                  hand today!</div>
-              <div class="container-3" style="position:absolute;width:100%;height:100vh;z-index:3">
+              <div class="container-3 font" style="position:absolute;width:100%;height:100vh;z-index:3">
                 <div class="text-h1 bg-transparent" :class="textColor" style="position:absolute;top:70;z-index:3">Lend a
                   hand today!
                 </div>
@@ -62,33 +59,20 @@
                     <div class="col q-pl-xl bg-transparent" style="padding:0px;">
                       <div class="bg-transparent"
                         style="max-width: 80vw; text-align: center; margin-left:auto; margin-right: auto">
-                        <div class="text-h1 text-bold text-white q-pb-md bg-transparent" style="text-shadow: #000 5px 2px 5px;"> Find an angel on Mortal Portal
-                        </div>
                         <div class="text-h5 text-italic text-bold text-white bg-transparent" style="text-shadow: #000 5px 2px 5px;">
-                        style="max-width: 550px; text-align: center; margin-left:auto; margin-right: auto">
-                        <div class="text-h2 text-bold text-primary q-pb-md bg-transparent"> Find an angel on Mortal Portal
+                          <div class="text-h2 text-bold text-white q-pb-md bg-transparent"> Find an angel on Mortal Portal</div>
+                          <div class="text-h6 text-italic text-primary bg-transparent">
+                            As a mortal, leverage on local residents to complete tasks you don't have time or knowledge to
+                            carry
+                            out.
+                            <br>
+                            Ask for more, with angels at your door!
+                          </div>
+                          <div class="text-h4 text-bold text-white q-pl-lg q-pr-lg q-pb-sm bg-transparent" style="text-shadow: #000 5px 2px 5px;"> What do you
+                            need
+                            help with today?
+                          </div>
                         </div>
-                        <div class="text-h6 text-italic text-primary bg-transparent">
-                          As a mortal, leverage on local residents to complete tasks you don't have time or knowledge to
-                          carry
-                          out.
-                          <br>
-                          Ask for more, with angels at your door!
-                        </div>
-                        <div class="text-h4 text-bold text-white q-pl-lg q-pr-lg q-pb-sm bg-transparent" style="text-shadow: #000 5px 2px 5px;"> What do you
-                        <div class="text-h5 text-bold text-primary q-pl-lg q-pr-lg q-pb-sm bg-transparent"> What do you
-                          need
-                          help with today?
-                        </div>
-                        <!-- <q-input class="rounded-pill bg-transparent" color="black" rounded outlined
-                      placeholder="Try searching for a task (e.g. 'help me buy groceries')">
-                      <template v-slot:prepend>
-                        <q-icon name="search" />
-                      </template>
-                      <template v-slot:append>
-                        <q-btn unelevated rounded color="primary" label="Search" class="bg-transparent" />
-                      </template>
-                    </q-input> -->
                       </div>
                     </div>
                   </div>
